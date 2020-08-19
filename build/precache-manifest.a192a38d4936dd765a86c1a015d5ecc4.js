8f35795e35d92fe07fad89fbbac72c3897750fe6
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-19 13:14:18 -0400

    init

---
+++ build/precache-manifest.a192a38d4936dd765a86c1a015d5ecc4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09adf47e22ed9d32f9ee323d6a3be7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a6d0298e542a90f956",
+    "url": "/static/css/main.34c9d91a.chunk.css"
+  },
+  {
+    "revision": "71ea73a85254237feb90",
+    "url": "/static/js/2.790cbd23.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.790cbd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a6d0298e542a90f956",
+    "url": "/static/js/main.48112ae5.chunk.js"
+  },
+  {
+    "revision": "e95a3050e98ec1b87237",
+    "url": "/static/js/runtime-main.52d2d5d2.js"
+  },
+  {
+    "revision": "0f8d9d40a243f4274f4f2e36fd059f6c",
+    "url": "/static/media/abode.0f8d9d40.jpg"
+  },
+  {
+    "revision": "d84d1b834b5b5216f0c5964aa7fffdb5",
+    "url": "/static/media/ceddy_resume.d84d1b83.pdf"
+  },
+  {
+    "revision": "cf4b70b43a023e0b6206842ae510051e",
+    "url": "/static/media/kayaksWh.cf4b70b4.jpg"
+  },
+  {
+    "revision": "75a71c3d1a2e6b619c81822f8eba2d92",
+    "url": "/static/media/linkedin2.75a71c3d.jpg"
+  },
+  {
+    "revision": "457bcafdd999a94e76a5385acd49a0b5",
+    "url": "/static/media/match_gamealt2.457bcafd.jpg"
+  },
+  {
+    "revision": "24168bfde8b0c64ceebe8eb478aa2b9d",
+    "url": "/static/media/pets.24168bfd.jpg"
+  },
+  {
+    "revision": "95734222389cd037d64ad13778769321",
+    "url": "/static/media/simon2.95734222.jpg"
+  },
+  {
+    "revision": "3068fc1aa05a2e4c0bee26f5d5887cc0",
+    "url": "/static/media/swirl_sm.3068fc1a.jpg"
+  }
+]);
